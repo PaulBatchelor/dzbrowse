7b6e5fba201f295ec6348627711b8740b32b1c76
Paul Batchelor <thisispaulbatchelor@gmail.com>
2024-12-21 13:19:08 -0500

    generator: nodes are displayed in nodelist

--- generate.py
+++ generate.py
@@ -1,11 +1,106 @@
 import sqlite3
 from pprint import pprint
 import json
+import re
+
+def shortname(namespace, name):
+    return re.sub("^" + namespace +"/", "", name)
+
+def create_connections(db, nodes):
+    connections = {}
+    def insert_connections(rows):
+        for left, right in rows:
+            assert(left is not None)
+            if left not in connections:
+                connections[left] = {}
+            assert(right is not None)
+            connections[left][right] = True
+    for nid in nodes.keys():
+        rows = db.execute(" ".join([
+            f"SELECT left, right FROM dz_connections",
+            f"WHERE left == {nid}",
+        ]))
+        insert_connections(rows)
+        rows = db.execute(" ".join([
+            f"SELECT left, right FROM dz_connections",
+            f"WHERE right == {nid}",
+        ]))
+        insert_connections(rows)
+    return connections
+
+def lookup_name_from_id(db, nid):
+    rows = db.execute(f"SELECT name FROM dz_nodes WHERE id == {nid}")
+    name = None
+    for row in rows:
+        name = row
+    return name
+
+def generate_node_data(nodes, connections, path, db, nid):
+    children = []
+    node = {}
+    lines = None
+    remarks = None
+    hyperlink = None
+    reference = None
+    flashcard = None
+
+    if nid in nodes:
+        node["name"] = shortname(path, nodes[nid])
+    else:
+        node["name"] = lookup_name_from_id(db, nid)
+
+    if lines:
+        node["lines"] = lines
+    if remarks:
+        node["remarks"] = remarks
+
+    if hyperlink: 
+        node["hyperlink"] = hyperlink
+
+    node["nid"] = nid
+
+    if reference:
+        node.reference = reference
+    
+    if flashcard:
+        node.flashcard = flashcard
+
+
+    return node, children
 
 def node_object(db, path, nodes, subgraphs):
     obj = {}
+    traversed = set()
+    connections = create_connections(db, nodes)
+    nodelist = []
 
-    obj["nodes"] = []
+    def traverse_node(nid):
+        if nid not in traversed:
+            traversed.add(nid)
+            node, children = generate_node_data(
+                nodes,
+                connections,
+                path,
+                db,
+                nid
+            )
+            nodelist.append(node)
+            return children
+        return []
+
+    def traverse_children(children):
+        for child in children:
+            traverse_node(child)
+
+    # skipping topsort for now, is it actually needed?
+    for nid in nodes:
+        if nid not in traversed:
+            children_nids = traverse_node(nid)
+            if len(children_nids) > 0:
+                traverse_children(children_nids)                
+
+
+    obj["nodes"] = nodelist
     obj["namespace"] = path
     obj["tree"] = []
     obj["remarks"] = []
@@ -89,7 +184,7 @@ def generate(dbpath):
 
 
     data_files = DataFiles()
-    generate_page_data(db, h, lookup, None, None, data_files)
+    generate_page_data(cur, h, lookup, None, None, data_files)
     data_files.close()
 
 generate("../recurse/a.db")
