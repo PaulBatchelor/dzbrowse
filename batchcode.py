f654f05bdaa5fe4d82b9be12011816e20456038b
Paul Batchelor <thisispaulbatchelor@gmail.com>
2025-01-13 21:15:31 -0500

    batchcode: codefiles.txt should be an arg

--- batchcode.py
+++ batchcode.py
@@ -1,8 +1,8 @@
 #! /usr/bin/env python3
 import import_code
 
-def run(top):
-    with open("codefiles.txt") as fp:
+def run(top, codefiles="codefiles.txt"):
+    with open(codefiles) as fp:
         for line in fp:
             file, path = line.split()[:2]
             file = f"{top}/{file}"
@@ -11,6 +11,11 @@ def run(top):
 if __name__ == "__main__":
     import sys
     top = "."
+    codefiles="codefiles.txt"
     if len(sys.argv) > 1:
         top = sys.argv[1]
-    run(top)
+
+    if len(sys.argv) > 2:
+        codefiles = sys.argv[2]
+
+    run(top,codefiles=codefiles)
